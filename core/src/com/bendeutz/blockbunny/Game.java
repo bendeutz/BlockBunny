964435c0b7e2b61fcc7982f404cb131dadbdc70d
bendeutz <benjamin.deutz@googlemail.com>
2015-03-20 09:38:12 +0100

    Game.java

--- core/src/com/bendeutz/blockbunny/Game.java
+++ core/src/com/bendeutz/blockbunny/Game.java
@@ -11,7 +11,7 @@ import com.bendeutz.blockbunny.handlers.MyInputProcessor;
 public class Game implements ApplicationListener{
 
 	
-	public static final String TITLE = "Block Bunny";
+	public static final String TITLE = "Block Bunny - Beta";
 	public static final int V_WIDTH = 320;
 	public static final int V_HEIGHT = 240;
 	public static final int SCALE = 2;
